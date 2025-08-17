031928b260a0fb153258c701cb4efddd11b8d039
ecabarga-web <ecabarga@gmail.com>
2025-08-17 12:08:29 -0400

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,9 @@
+export const config = {
+  runtime: 'nodejs20.x',
+  regions: ['iad1'],
+  maxDuration: 10
+};
+
 export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json({ ok: true, time: new Date().toISOString() });
